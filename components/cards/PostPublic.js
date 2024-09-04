8ce98d15c1533d819dfdce41edb7611ae521cada
Godspower <chimabest70@gmail.com>
2024-09-04 15:20:04 +0100

    first commit

---
+++ components/cards/PostPublic.js
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import renderHTML from "react-render-html";
+import PostImage from "../images/PostImage";
+import {
+  
+} from "@ant-design/icons";
+import { UserContext } from "../../context";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const PostPublic = ({
+  post,
+  
+}) => {
+  const [state] = useContext(UserContext);
+  const router = useRouter();
+
+  return (
+    <>
+      {post && post.postedBy && (
+        <div key={post._id} className="card mb-5">
+          <div className="card-footer">
+            {post.image && <PostImage url={post.image.url} />}
+              <div className="card-body truncate">{renderHTML(post.content)}</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default PostPublic;
